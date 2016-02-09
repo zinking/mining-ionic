0157f39d52ffd8629f4adcbe809357e80a70ce18
zhen wang <zinking3@gmail.com>
2016-02-09 16:12:36 +0800

    star handlers

---
+++ www/js/content/controllers/OpmlStarListController.js
@@ -0,0 +1,57 @@
+angular.module('mining.content')
+    .controller('OpmlStarListCtrl', function($scope, $ionicLoading, $state, $ionicPopup, $ionicPopover,
+                                             AccountDataService, ContentDataService, SessionService, UserDataModel) {
+        $scope.viewModel = {
+            isBusy:false,
+            //totalUnReadCount: 0,
+            omplsList : []
+        };
+
+
+        $scope.viewModel.isBusy = true;
+
+        ContentDataService.listStarFeed().then(
+            function(){
+                $scope.viewModel.isBusy = false;
+                var opmlList = globalUserData.StarOpml;
+                _.each(opmlList, function(opml){
+                    opml.isFolder = 'Outline' in opml;
+                    opml.isOpen = false;
+                });
+                $scope.viewModel.opmlsList = opmlList;
+                //$scope.viewModel.totalUnReadCount = globalUserData.totalUnReadCount;
+            },
+            function(){
+                $scope.viewModel.isBusy = false;
+                $ionicPopup.alert({
+                    title: 'Loading Failed,fallback on cache'
+                });
+                ContentDataService.loadLocalUserData();
+                $scope.viewModel.opmlsList = globalUserData.Opml;
+            }
+        );
+
+        $scope.openFolder = function (opml) {
+            opml.isOpen = !opml.isOpen;
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            $scope.$broadcast('scroll.resize');
+        };
+
+        $scope.openOpmlFeed = function ( fo ){
+            $state.go('feed',{opmlFeed:fo});
+        };
+
+        $scope.openAddPanel = function (){
+            $state.go('source');
+        };
+
+        $scope.openAddFeed = function(){
+            $state.go('addfeed');
+        };
+
+        $scope.openBulkFeed = function(){
+            $state.go('bulkfeed');
+            $scope.closePopover();
+        };
+
+    });
